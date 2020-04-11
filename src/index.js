a8d17e8c4d8b93ae924e5bb12003da0b62590f91
Si3ver <izhouwl@163.com>
2020-04-12 00:51:15 +0800

    定义并使用装饰器, 劫持类的函数成员

--- src/index.js
+++ src/index.js
@@ -1,19 +1,36 @@
-class Animal {
-  name() {
-    return 'animal'
-  }
-  say() {
-    return `I'm ${this.name()}`
+function log(target) {
+  const desc = Object.getOwnPropertyDescriptors(target.prototype) // 获取类的 {属性:描述符}
+
+  for (const key of Object.keys(desc)) {
+    if (key === 'constructor') {
+      continue
+    }
+    const func = desc[key].value
+    if ('function' === typeof func) {
+      Object.defineProperty(target.prototype, key, {
+        value(...args) {
+          console.log('before ' + key)
+          const ret = func.apply(this, args)
+          console.log('after ' + key)
+          return ret
+        }
+      })
+    }
   }
 }
 
-class Dog extends Animal {
-  food = 'bone'
-  name() {
-    return 'Dog'
+@log
+class Numberic {
+  PI = 3.1415926
+
+  add(...nums) {
+    return nums.reduce((p, n) => (p + n), 0)
+  }
+  multiply(...nums) {
+    return nums.reduce((p, n) => (p * n), 1)
   }
 }
 
-document.write(new Dog().say())
-console.log(Dog.prototype.constructor)
-console.log(new Dog() instanceof Animal)
+var calctor = new Numberic()
+console.log(calctor.add(4, 5, 6)) // before add\nafter add\n15
+console.log(calctor.multiply(4, 5, 6)) // before multiply\nafter multiply\n120
