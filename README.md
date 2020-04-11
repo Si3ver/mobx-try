51ad3b11d7e30abee42a94ce0f55a763bb4ae992
Si3ver <izhouwl@163.com>
2020-04-12 01:26:24 +0800

    mobx observable

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 [babel@7.9](https://babeljs.io/docs/en/usage)
 
 + 响应式
+
+## MobX 常用 API
+
+**1. 可观察的数据 observable**
--- src/index.js
+++ src/index.js
@@ -1,61 +1,35 @@
-function log(target) {
-  const desc = Object.getOwnPropertyDescriptors(target.prototype) // 获取类的 {属性:描述符}
+import { observable, isArrayLike } from 'mobx'
 
-  for (const key of Object.keys(desc)) {
-    if (key === 'constructor') {
-      continue
-    }
-    const func = desc[key].value
-    if ('function' === typeof func) {
-      Object.defineProperty(target.prototype, key, {
-        value(...args) {
-          console.log('before ' + key)
-          const ret = func.apply(this, args)
-          console.log('after ' + key)
-          return ret
-        }
-      })
-    }
-  }
+// Array
+let arr = observable(['a', 'b', 'c'])
+console.log(arr, Array.isArray(arr), isArrayLike(arr))
+
+// Object
+let obj = observable({ a: 1, b: 2 })
+console.log(obj)
+
+// hash Map
+let map = observable(new Map())
+map.set('a', 1)
+console.log(map)
+
+// Base Type
+var num = observable.box(20),
+  str = observable.box('hello'),
+  bool = observable.box(true)
+console.log(num, str, bool, num.get(), str.get(), bool.get())
+num.set(50)
+str.set('world')
+bool.set(false)
+console.log(num.get(), str.get(), bool.get())
+
+class Store {
+  @observable array = []
+  @observable obj = {}
+  @observable map = new Map()
+
+  @observable string = 'hello'
+  @observable number = 20
+  @observable bool = false
 }
-
-function readonly(_target, _key, descriptor) {
-  descriptor.writable = false
-}
-
-/**
- * 类方法参数校验器
- */
-function validate(_target, _key, descriptor) {
-  const func = descriptor.value
-  descriptor.value = function (...args) {
-    for (let num of args) {
-      if ('number' !== typeof num) {
-        throw new Error(`"${num}" is not Number`)
-      }
-    }
-
-    return func.apply(this, args)
-  }
-}
-
-@log
-class Numberic {
-  @readonly PI = 3.1415926
-
-  @validate
-  add(...nums) {
-    return nums.reduce((p, n) => (p + n), 0)
-  }
-  @validate
-  multiply(...nums) {
-    return nums.reduce((p, n) => (p * n), 1)
-  }
-}
-
-var calctor = new Numberic()
-console.log(calctor.add(4, 5, 6)) // before add\nafter add\n15
-console.log(calctor.multiply(4, 5, 6)) // before multiply\nafter multiply\n120
-// calctor.PI = 1 // index.js:41 Uncaught TypeError: Cannot assign to read only property 'PI' of object '#<Numberic>
-console.log(calctor.add(4, 'ab', 6)) // before add\nUncaught Error: "ab" is not Number
-
+console.log(Store)
